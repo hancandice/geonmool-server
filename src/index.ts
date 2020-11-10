2be46ea3aad7f213cb7709a5730d41db1fcd6385
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-11-10 13:27:04 +0900

    IMPORT redis

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,48 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig); // connect to database
   await orm.getMigrator().up(); // run migration
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({
+    context: ({ req, res }) => ({
       em: orm.em,
+      req,
+      res,
     }),
   });
 
