ea22044ab96221889aa77a8524ba3b4bf1624d95
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-11-13 16:03:30 +0900

    Update user.ts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -51,14 +51,14 @@ export class UserResolver {
         ],
       };
     }
-
-    const userId = await redis.get(FORGOT_PASSWORD_PREFIX + token);
+    const key = FORGOT_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
     if (!userId) {
       return {
         errors: [
           {
             field: "token",
-            message: "token expired",
+            message: "token has already expired",
           },
         ],
       };
@@ -79,6 +79,8 @@ export class UserResolver {
 
     user.password = await argon2.hash(newPassword);
     await em.persistAndFlush(user);
+
+    await redis.del(key);
 
     // log in user after change password
     req.session.userId = user.id;
@@ -108,7 +110,7 @@ export class UserResolver {
 
     await sendEmail(
       email,
-      `<a href="http://localhost:3000/reset-password/${token}">reset password</a>`
+      `<a href="http://localhost:3000/reset-password/${token}">Reset Password</a>`
     );
     return true;
   }
